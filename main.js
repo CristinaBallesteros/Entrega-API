aeec2459f83c652aa71d5294538331139cec0a58
CristinaBallesteros <123888122+CristinaBallesteros@users.noreply.github.com>
2023-05-30 12:28:40 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
     var fileInput = document.getElementById('fileInput');
-    var videoPlayer = document.getElementById('videoPlayer');
-    var playButton = document.getElementById('playButton');
-    var pauseButton = document.getElementById('pauseButton');
-    var volumeSlider = document.getElementById('volumeSlider');
+    var video = document.getElementById('video');
+    var play = document.getElementById('play');
+    var pause = document.getElementById('pause');
+    var volumeup = document.getElementById('volume+');
+    var volumedown = document.getElementById('volume-');
     var loadingMessage = document.getElementById('loadingMessage');
-  
+
     fileInput.addEventListener('change', function(e) {
       var file = e.target.files[0];
       if (file && file.type.startsWith('video/')) {
         var fileURL = URL.createObjectURL(file);
-        videoPlayer.src = fileURL;
+        video.src = fileURL;
         showLoadingMessage();
       } else {
-        alert('Por favor, seleccione un archivo de vídeo válido.');
+        alert('El archivo selecionado no es válido.Por favor, seleccione un archivo de vídeo válido.');
       }
     });
   
-    playButton.addEventListener('click', function() {
-      videoPlayer.play();
+    play.addEventListener('click', function() {
+      video.play();
     });
   
-    pauseButton.addEventListener('click', function() {
-      videoPlayer.pause();
+    pause.addEventListener('click', function() {
+      video.pause();
     });
   
-    volumeSlider.addEventListener('input', function() {
-      videoPlayer.volume = volumeSlider.value;
+    volumeup.addEventListener('click', function() {
+      changeVolume(0.1);
     });
+
+    volumedown.addEventListener('click', function() {
+      changeVolume(-0.1);
+    });
+
+    function changeVolume(delta) {
+      var newVolume = video.volume + delta;
+      newVolume = Math.max(0, Math.min(1, newVolume)); 
+      video.volume = newVolume;
+    }
   
-    videoPlayer.addEventListener('loadedmetadata', hideLoadingMessage);
+    video.addEventListener('loadedmetadata', hideLoadingMessage);
   
     function showLoadingMessage() {
       loadingMessage.style.display = 'block';
